e38622162c5dd73a0e12008fdbfd3aa5214b9e4c
Saha <ibrahimsaha98@gmail.com>
2026-06-23 10:26:12 +0000

    Scaffold Spring Boot backend and connect to Neon Postgres

---
+++ leximate_backend/src/main/java/com/leximate/leximate_backend/LeximateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leximate.leximate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeximateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeximateBackendApplication.class, args);
+	}
+
+}
